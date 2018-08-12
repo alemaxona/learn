28606c7aa8534cd632b507d06916395e1ccb998b
Maxim <41756364+alemaxona@users.noreply.github.com>
2018-08-12 16:06:14 +0300

    Add home work

---
+++ dz1.py
@@ -0,0 +1,120 @@
+# Home work++ for video1
+score = 0
+answer_attempts = 0
+print("""\n
+		It\'s 5 questions!\n 
+		You have 20 attempts for all question.\n
+		One right answer = +1 Score.\n
+		One false answer = -1 Score.\n
+		4+ Score - You win!\n
+		1-2 Scores - You Lost!\n
+		3 Scores - It\'s a draw\n
+		Good Luck! """)
+while answer_attempts < 20:
+	answer = input('3 > 2? y/n:')
+	answer_attempts = answer_attempts + 1
+	if answer != 'y' and answer != 'n' and answer != 'Y' and answer != 'N': 
+		print('Please enter Y/y or N/n')
+	else: 
+		if answer == 'y' or answer == 'Y':
+			score = score + 1
+			print('Answer accepted!')
+			break
+		else:
+			if answer == 'n' or answer =='N':
+				if score == 0:
+					score = 0
+					break
+				else:
+					score = score - 1
+					print('Answer accepted!')
+					break
+while answer_attempts < 20:
+	answer = input('What is my name? Maxim or Alex? y/n:')
+	answer_attempts = answer_attempts + 1
+	if answer == 'maxim' or answer == 'MAXIM' or answer =='Maxim':
+		score = score + 1
+		print('Answer accepted!')
+		break
+	else:
+		if answer == 'alex' or answer =='ALEX' or answer == 'Alex':
+			if score == 0:
+				score = 0
+				break
+			else:
+				score = score - 1
+				print('Answer accepted!')
+				break
+		else:
+			 if score == 0: 
+			 	score = 0
+			 else:	
+			 	score = score - 1
+			 	print ('My name not ' + str(answer))
+			 break
+while answer_attempts < 20:
+	answer = input('What my love Phone? y/n:')
+	answer_attempts = answer_attempts + 1
+	if answer == 'Iphone' or answer == 'iPhone' or answer =='IPHONE':
+		score = score + 1
+		print('Answer accepted!')
+		break
+	else:
+		if score == 0: 
+			score = 0
+		else:	
+			score = score - 1
+			print ('My Love Phone not ' + str(answer))
+			break
+while answer_attempts < 20:
+	answer = input('8 * 8 =')
+	answer_attempts = answer_attempts + 1
+	if answer == str('64'):
+		score = score + 1
+		print('Answer accepted!')
+		break
+	else:
+		if score == 0: 
+			score = 0
+		else:	
+			score = score - 1
+			print ('8 * 8 != ' + str(answer) + '!')
+			break
+while answer_attempts < 20:
+	answer = input('The biggest animal?  ')
+	answer_attempts = answer_attempts + 1
+	if answer == 'Elephant':
+		score = score + 1
+		print('Answer accepted!')
+		break
+	else:
+		if score == 0: 
+			score = 0
+		else:	
+			score = score - 1
+			print (str(answer) + 'is not the biggest animal' + '!')
+			break
+
+
+if score > 4:
+	print('You win!' + 'Your score: ' + str(score) +'!' + ' Answer attempts: ' + str(answer_attempts) + '!')
+else:
+	if score < 2:
+		print('You lost!' + 'Your score: ' + str(score) +'!' + ' Answer attempts: ' + str(answer_attempts) + '!')
+	else:
+		print('It\'s a draw' + 'Your score: ' + str(score) +'!' + ' Answer attempts: ' + str(answer_attempts) + '!')
+
+
+#Numbers are multiple of 5
+list_multiple = []
+number = 1
+user_number = input('Please enter number other than 0: ')
+while number <= int(user_number):
+	if number % 5 == 0:
+		print('Number - ' + str(number) + ' is a multiple of 5')
+		list_multiple.append(number)
+		number = number  + 1
+	else:
+		print('Number -  ' + str(number) + ' is not a multiple of 5!')
+		number = number  + 1
+print ('Numbers are multiple of 5: ' + str(list_multiple))
---
+++ notes.py
@@ -0,0 +1,165 @@
+#
+tuple1 = (1, 2, 'string', 'one more', )
+new_tuple = tuple()
+for item in tuple1:
+    # if not isinstance(item, (str, unicode)):  # or `basestring`
+    if not isinstance(item, str): # Если этот елемент не строка
+        new_tuple += (item, ) # new_tuple = new_tuple + (item, )
+print(new_tuple)
+(1, 2)
+
+
+#
+list1 = [1, 2 ]
+list2 = [7, 8 ]
+list1.append(lsit2) # Мутация, append - добавление в список 1 елемент
+print(list1)
+[1, 2, [7, 8]]
+
+list1.extend(list2) # Мутация, extend - добавление нескольких елементов
+print(list1)
+[1, 2, 7, 8]
+
+list1.insert(1, 'str') # Не стоит использовать! 1 здесь позиция в list
+list1.pop(1) # Delete element for index
+list1.remove('2') # Delete element for value
+del list1[0] # Delete element for index !Некрасивая версия pop!
+
+
+#
+for row in multi:
+    for element in row:
+
+#
+list(range(1, 10, 1)) # Генератор. 3 параметр - шаг
+[1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# Все ключи в словаре уникальные!
+# Неизменяемый тип данных не может быть ключем!
+s = {'s': 's', 1: 'str', None: 3}
+s.update({2: 'q'}) #Мутация в словаре. Добавить пару ключ - значение
+s[2] = None
+s[ma] # Выдаст ошибку, так как ключа такого нет
+s.get(ma) # Ошибку не выдаст, но и результат тоже.
+s.keys() # Получить ключи словаря
+s.values() # Получить елементы словаря
+for k in s:
+	print(key, s[k])
+s s
+1 str
+None 3
+
+for k, v in s.items():
+	print(k, v)
+s s
+1 str
+None 3
+
+s.pop('s') # Удаляет и ключ и значение
+
+#
+try:
+    print(1 / 0)
+except Exception:  # it is almost the same as just `except:`
+    print('0!!')
+#
+try:
+    1 / 0
+except ValueError: # Не сработает так как ошибка ZeroDivisionError а не ValueError!
+    print('0!!')
+
+try:
+    1 / 0
+except ZeroDivisionError: # Сработает так как ошибка ZeroDivisionError!
+    print('0!!')
+#
+try:
+    1 / int(input('x: '))
+except ZeroDivisionError:
+    print('/0')
+except TypeError:
+    print('WrongValue')
+
+#
+try:
+    print(1 / 0)
+except ZeroDivisionError as e: # В переменную!
+    print('Exception! Stop it!')
+    print(e)
+#
+raise IndexError('Hi!') # Выкинуть ошибку
+#
+try:
+    raise TypeError('Some message')
+except TypeError as e:
+    print(e)
+
+try:
+    print('Fine')
+except KeyError:
+    print('Nope')
+else:
+    print('Else clause')
+Fine
+Else clause
+
+try:
+    print(1 / 0)
+except ZeroDivisionError:
+    print('0!')
+finally: # finally всегда работает!
+    print('I will be called in the end!')
+0!
+I will be called in the end!
+
+try:
+    print('try')
+except ValueError:
+    pass #Игнорирует ошибку!
+    print('else')
+finally:
+    print('finally')
+
+
+# FUNCTIONS Всегда с маленькой буквы!
+
+def return_none(x):
+    print('Param was', x) #Если у функции нет return то она возвращает None!
+test = return_none(6)
+Param was 6
+
+print(test)
+None
+
+def sum_numbers(n1, n2, n3):
+    print(n1, n2, n3, 'summing...')
+    return n1 + n2 + n3
+
+total = sum_numbers(7, 0.3, 0.3)
+7 0.3 0.3 summing...
+
+print(total)
+7.6
+
+
+def sum_my(a, b=3): # Значение по умолчанию, если аргумент не ввели!
+    print(a + b)
+# >>> sum_my(1)
+4
+
+def my_numbers(*args): # Сколь угодно аргументов!
+
+def any_keywords(**kwargs): # Сколь угодно елементов со значением по умолчанию!
+    print(kwargs, type(kwargs))
+
+any_keywords(k=1, some=2, wo=3)
+{'k': 1, 'some': 2, 'wo': 3} <class 'dict'>
+
+#
+print('Word', end = '!')
+Word!>>>
+
+print('Word', end = '\n')
+Word
+>>>
+
