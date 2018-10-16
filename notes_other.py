9f2434d5803f12860ad13fea196d6bd47e7b308b
Max <alemaxona@bk.ru>
2018-10-16 11:34:15 +0300

    Add notes

--- notes_other.py
+++ notes_other.py
@@ -36,8 +36,10 @@ for i in range(len(a)):
 
 
 # Встроенные объекты. (list, int, dict, len...)
+
 import builtins  # Можно не импортировать!
 print(dir(__builtins__))
+
 
 # Короткая запись - Списковые выражения:
 s = [w for w in range(1, 14)]
@@ -49,7 +51,6 @@ print(s)
 # [2, 4, 6, 8, 10, 12]
 
 
-#
 import os
 print(os)
 # <module 'os' from '/usr/local/Cellar/python/3.7.0/Frameworks/Python.framework/Versions/3.7/lib/python3.7/os.py'>
@@ -94,9 +95,9 @@ isinstance(c,tuple)
 # True
 
 #  Если словарь пустой то возвращается False!
-bool({}) # По сути идет сравнение длины, если длина = 0, то возвращается False!
+bool({})  # По сути идет сравнение длины, если длина = 0, то возвращается False!
 # False
-bool ({'Max': 5})
+bool({'Max': 5})
 # True
 
 # Сравнение едит по ключам!
@@ -107,7 +108,7 @@ bool ({'Max': 5})
 
 
 # Короткая запись условия
-value  = 2
+value = 2
 
 if value > 0:
     print(str(value))
@@ -139,19 +140,19 @@ first, *second = (1, 2, 3, 4, 5)
 print(first, second)
 # 1 [2, 3, 4, 5]
 
-*f,s = 'Moscow'
+*f, s = 'Moscow'
 print(f)
 # ['M', 'o', 's', 'c', 'o']
 
 
 #
-print('Word', end = '!')
+print('Word', end='!')
 # Word!>>>
 
-print('Word', end = '\n')
+print('Word', end='\n')
 # Word
 # >>>
-print('Max', end='!') #Не переходит на новую строку
+print('Max', end='!')  # Не переходит на новую строку
 print('Max', end='\n')
 
 
@@ -171,53 +172,50 @@ tuple1 = (1, 2, 'string', 'one more', )
 new_tuple = tuple()
 for item in tuple1:
     # if not isinstance(item, (str, unicode)):  # or `basestring`
-    if not isinstance(item, str): # Если этот елемент не строка
-        new_tuple += (item, ) # new_tuple = new_tuple + (item, )
+    if not isinstance(item, str):  # Если этот елемент не строка
+        new_tuple += (item, )  # new_tuple = new_tuple + (item, )
 print(new_tuple)
-#(1, 2)
+# (1, 2)
 
 #
 list1 = [1, 2 ]
 list2 = [7, 8 ]
 list1.append(list2) # Мутация, append - добавление в список 1 елемент
 print(list1)
-#[1, 2, [7, 8]]
+# [1, 2, [7, 8]]
 
 list1.extend(list2) # Мутация, extend - добавление нескольких елементов
 print(list1)
-#[1, 2, 7, 8]
+# [1, 2, 7, 8]
 
-list1.insert(1, 'str') # Не стоит использовать! 1 здесь позиция в list
-#list1.pop(1) # Delete element for index
-#list1.remove('2') # Delete element for value
-#del list1[0] # Delete element for index !Некрасивая версия pop!
+list1.insert(1, 'str')  # Не стоит использовать! 1 здесь позиция в list
+# list1.pop(1)  # Delete element for index
+# list1.remove('2')  # Delete element for value
+# del list1[0]  # Delete element for index !Некрасивая версия pop!
 
 
-for row in multi:
-    for element in row:
-
-list(range(1, 10, 1)) # Генератор. 3 параметр - шаг
-#[1, 2, 3, 4, 5, 6, 7, 8, 9]
+list(range(1, 10, 1))  # Генератор. 3 параметр - шаг
+# [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 # Все ключи в словаре уникальные!
 # Неизменяемый тип данных не может быть ключем!
-s = {'a' : 'a', 1 : 'str', None : 3}
-s.update({2: 'q'}) # Мутация в словаре. Добавить пару ключ - значение
+s = {'a': 'a', 1: 'str', None: 3}
+s.update({2: 'q'})  # Мутация в словаре. Добавить пару ключ - значение
 s[2] = None
-#s[ma] # Выдаст ошибку, так как ключа такого нет
-#s.get(ma) # Ошибку не выдаст, но и результат тоже.
-#s.keys() # Получить ключи словаря
-#s.values() # Получить елементы словаря
+# s[ma] # Выдаст ошибку, так как ключа такого нет
+# s.get(ma) # Ошибку не выдаст, но и результат тоже.
+# s.keys() # Получить ключи словаря
+# s.values() # Получить елементы словаря
 for k in s:
     print(k, s[k])
-#s s
-#1 str
-#None 3
+# s s
+# 1 str
+# None 3
 
 for k, v in s.items():  # ==
     print(k, v)
-#s s
-#1 str
-#None 3
+# s s
+# 1 str
+# None 3
 
-#s.pop('s') # Удаляет и ключ и значение
+# s.pop('s')  # Удаляет и ключ и значение
