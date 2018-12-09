4b18093372962be75aff46af301f1b0113bb4ac9
alemaxona <alemaxona@gmail.com>
2018-12-09 21:34:20 +0300

    Added notes.

--- notes_flask.py
+++ notes_flask.py
@@ -1,1 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Framework - другое название библиотеки
+# Flask Самый простой фрэймворк(micro (значит - очень маленький) framework) для web разработки (Соболев Н.)
+# Django - огромный, полноценный фрэймворк.
+
+
 __author__ = 'alemaxona'
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return 'hello world!'
+
+
+if __name__ == '__main__':
+    app.run()
