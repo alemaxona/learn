7c45a7bf3cd13ad0d9a4de2936364910ea8dc5a9
alemaxona <alemaxona@gmail.com>
2019-01-07 16:09:48 +0300

    Added notes

--- notes_json.py
+++ notes_json.py
@@ -1,17 +1,12 @@
 # -*- coding: utf-8 -*-
 
 
-# Протокол WSGI - Web Server Gateway Interface
-# Используется для общения Python-программы и сервера (Nginx, Apache) (PEP-3333)
-# Используетс всеми web-фреймворками
-
-
 # JSON - JavaScript Object Notation
 # Как собособ представить объект ввиде текса
 # Двойные кавычки
 # Примеры того, как выглядит формат JSON
 
-# { "name":"Max"}  # Пробелы необязательны!
+# { "name": "Max"}  # Пробелы необязательны!
 
 # {
 #     "people":{ "name": "Max", "age": 30 }
@@ -33,6 +28,16 @@ data = open_file(filename)  # Сейчас data - это строка!
 print(data, 'TYPE - ', type(data), '\n')
 
 # При преобразовании заменяет двойные кавычки на одинарные, null на None и т.д.
-obj = json.loads(data)  # Преобразует строку в дикт
+obj = json.loads(data)  # Преобразует строку в словарь
+# obj = json.dumps(data)  # Преобразует словарь в строку
 print(obj, 'TYPE - ', type(obj), '\n')
 print(obj['none'], obj['object']['key'])
+
+# Можно использовать jsonify из модуля flask.json
+
+from flask.json import jsonify
+
+
+def my_dict():
+    my_locale = {'ru': 'russian', 'en': 'english', 'it': 'italian'}
+    return jsonify(my_locale)
