2a04f6895704bca3bada60e26c44384f5a5da2ff
alemaxona <alemaxona@gmail.com>
2019-01-09 21:26:16 +0300

    Added notes

--- notes_files_and_managers_contexts.py
+++ notes_files_and_managers_contexts.py
@@ -112,37 +112,3 @@ f.close()
 from os import *
 source = r'C:\Project\dir_create_with_python'  # r - отключает механизм экранирования
 makedirs(source)
-
-
-# JSON - JavaScript Object Notation
-# Как собособ представить объект ввиде текса
-# Двойные кавычки
-# Примеры того, как выглядит формат JSON
-
-# { "name":"Max"}  # Пробелы необязательны!
-
-# {
-#     "people":{ "name": "Max", "age": 30 }
-# }
-
-# { "people":["Max", 30] }
-
-
-import json
-filename = r'C:\Project\learn\files\json_test.json'
-
-
-def open_file(file):
-    with open(file) as f:
-        return f.read()
-
-
-data = open_file(filename)  # Сейчас data - это строка!
-print(data, 'TYPE - ', type(data), '\n')
-
-# При преобразовании заменяет двойные кавычки на одинарные, null на None и т.д.
-obj = json.loads(data)  # Преобразует строку в дикт
-print(obj, 'TYPE - ', type(obj), '\n')
-print(obj['none'], obj['object']['key'])
-
-
