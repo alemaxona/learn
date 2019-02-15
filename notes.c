781b9f5534af915dd5989825e5381e788a55582e
Max <alemaxona@bk.ru>
2019-02-15 12:15:15 +0300

    Added notes

--- notes.c
+++ notes.c
@@ -78,3 +78,95 @@ int main()
 // c = (float)a/b  // На время выполнения делает переменную a - float
 // printf("%f", c)
 
+//------------------------------------------------------
+// Б. Керниган, Д. Ритчи. Язык программирования Си:
+// Прочитать разделы 2.5 и 2.6
+// Упражнение 2.2
+
+// Написать программу c функцией реализующую бинарную операцию исключающего логического ИЛИ (XOR). 
+// Программа должна протестировать работу функции на корректность и вывести в консоль полные результаты проверки. 
+// При написании использовать заголовочный файл stdbool.h. 
+// Пример формата вывода результатов проверки: 
+// 	XOR (true, false) = true
+
+// (Опционально) Изменить программу на слайде 6 так, 
+// чтобы рассчитанное среднее сошлось с ожидаемым и составляло 10.1. 
+// Размерность массива и количество итераций изменять нельзя.
+
+//??
+// (Опционально) Написать программу, выводящую на экран самое большое число из базовых типов.
+
+
+// #include <stdio.h>
+// #define ARRAY_SIZE 10
+// #define VALUE 10.1
+
+// /* Returns the mean value of the array */
+// float mean(float array[], int size) {
+// 	float total = 0.0;
+// 	for (char i = 0; i < size; i++) {
+// 		total += array[i];
+// 		printf("array[%hhu] = %f and total is %f\n", i, array[i], total);
+// 	}
+// 	if (size != 0)
+// 		return total / size;
+// 	else
+// 		return 0.0;
+// }
+
+// int main(void) {
+// 	float array[ARRAY_SIZE];
+// 	float avg;
+// 	size_t i;
+// 	for (i = 0; i < ARRAY_SIZE; i++)
+// 		array[i] = VALUE;
+
+// 	avg = mean(array, ARRAY_SIZE);
+// 	printf("mean is %f\n", avg);
+// 	if (avg == array[0]) {
+// 		printf("array[0] is the mean\n");
+// 	}
+// 	else {
+// 		printf("array[0] is not the mean\n");
+// 	}
+// 	getchar();
+// 	return 0;
+// }
+
+
+
+
+// // Также сделать черз формулу
+// #include <stdio.h>
+// #include <stdbool.h>
+
+// bool XOR(bool a, bool b) {
+// 	bool c = a ^ b;
+// 	return c;
+
+// }
+
+// void main() {
+
+// 	printf("XOR(true, true): %i\n", XOR(true, true));
+// 	printf("XOR(true, false): %i\n", XOR(true, false));
+// 	printf("XOR(false, true): %i\n", XOR(false, true));
+// 	printf("XOR(false, false): %i\n", XOR(false, false));
+
+// 	getchar();
+// 	return;
+// }
+
+
+#include <stdio.h>
+
+void main() {
+    
+    float i = 10.1;
+    float arr [3] = {0.3, 0.2, 0.7};
+    for(int a = 0; a < 3; a++) {
+        i += arr[a];
+    }
+    printf("%f\n", i);
+    return;
+}
